33db7b862a77907c96ed22e55c0edc63196b779f
behnam1369 <behnam.aghaali@yahoo.com>
2022-12-26 16:38:26 +0330

    Add PR routes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Mission from "./pages/Mission/Mission";
+import PR from "./pages/PR/PR";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/users/:iduser/missions/new" element={<Mission />} />
+          <Route path="/users/:iduser/mission/new" element={<Mission />} />
           <Route
             path="/users/:iduser/missions/:idmission"
             element={<Mission />}
           />
+          <Route path="/users/:iduser/pr/:idpr" element={<PR />} />
+          <Route path="/users/:iduser/pr/new" element={<PR />} />
           <Route path="*" element={<h1>404</h1>} />
         </Routes>
       </BrowserRouter>
