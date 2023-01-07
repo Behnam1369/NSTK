cf8009e08e2060f5d1ecf57517c54b0393db6b0c
behnam1369 <behnam.aghaali@yahoo.com>
2023-01-07 17:27:34 +0330

    Add Work mission payment routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Mission from "./pages/Mission/Mission";
+import MissionPayments from "./pages/Mission/MissionPayments";
 import PR from "./pages/PR/PR";
 
 function App() {
@@ -9,8 +10,16 @@ function App() {
         <Routes>
           <Route path="/users/:iduser/mission/new" element={<Mission />} />
           <Route
+            path="/users/:iduser/mission/:idmission"
+            element={<Mission />}
+          />
+          <Route
             path="/users/:iduser/missions/:idmission"
             element={<Mission />}
+          />
+          <Route
+            path="/users/:iduser/missions/:idmission/missioners/:idmissioner/payments"
+            element={<MissionPayments />}
           />
           <Route path="/users/:iduser/pr/:idpr" element={<PR />} />
           <Route path="/users/:iduser/pr/new" element={<PR />} />
