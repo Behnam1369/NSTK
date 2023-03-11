4206bb1a5b5adb8eff009c1c3358b2979ae9b5b4
behnam1369 <behnam.aghaali@yahoo.com>
2023-03-11 10:51:49 +0330

    Create pattern item component

---
+++ src/pages/PiPattern/PatternItem.js
@@ -0,0 +1,122 @@
+import {
+  useEffect,
+  useRef,
+  useState,
+  forwardRef,
+  useImperativeHandle,
+} from "react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+import style from "./PatternItem.module.scss";
+
+const PatternItem = forwardRef(
+  ({ title, text, changeTitle, changeText, addItem, pi, onEdit }, ref) => {
+    const [editing, setEditing] = useState(false);
+    const [spannedText, setSpannedText] = useState(spanned(text));
+    const quillRef = useRef(null);
+
+    const evalTExt = text.replace(/{([^}]+)}/g, (match, expression) => {
+      try {
+        const result = eval(expression);
+        return result;
+      } catch (error) {
+        return expression;
+      }
+    });
+
+    useEffect(() => {
+      setSpannedText(spanned(text));
+    }, [text]);
+
+    const handleChangeText = (value) => {
+      const unSpannedValue = unSpanned(value);
+      if (unSpannedValue !== text) {
+        changeText(unSpannedValue);
+        setSpannedText(value);
+      }
+    };
+
+    const handleInsertExpression = (expression) => {
+      const quill = quillRef.current.getEditor();
+      // const selectionIndex = quill.getSelection();
+      // // console.log(quillRef.current);
+      if (selection) {
+        quill.deleteText(selection.index, selection.length);
+        quill.insertText(selection.index, `{${expression}}`);
+        quill.setSelection(selection.index + expression.length + 2);
+      }
+    };
+
+    useImperativeHandle(ref, () => ({
+      handleInsertExpression,
+      quillRef,
+    }));
+
+    const handleAdd = (e) => {
+      e.preventDefault();
+      addItem();
+    };
+
+    const handleEdit = () => {
+      setEditing(!editing);
+      onEdit();
+    };
+
+    const [selection, setSelection] = useState(null);
+    const handleBlur = (e) => {
+      setSelection(e);
+      console.log(e);
+    };
+
+    const handleUp = (e) => {
+      e.preventDefault();
+    };
+
+    return (
+      <div className={style.main}>
+        <div>
+          <input
+            type="text"
+            value={title}
+            onChange={changeTitle}
+            placeholder="title"
+          />
+          <div>
+            <button onClick={() => handleEdit()}>
+              {editing ? "Confirm" : "Edit"}
+            </button>
+            <button onClick={(e) => handleUp(e)}>Move Up</button>
+          </div>
+        </div>
+        {editing && (
+          <ReactQuill
+            theme="snow"
+            value={spannedText}
+            onChange={(val) => handleChangeText(val)}
+            ref={quillRef}
+            onBlur={(e) => handleBlur(e)}
+            onFocus={onEdit}
+          />
+        )}
+        {!editing && <div dangerouslySetInnerHTML={{ __html: evalTExt }} />}
+        <div class={style.add}>
+          <button onClick={(e) => handleAdd(e)}>Add Item</button>
+        </div>
+      </div>
+    );
+  }
+);
+
+export default PatternItem;
+
+const spanned = (t) => {
+  return t.replace(/{([^}]+)}/g, `<span style="color: blue;">{$1}</span>`);
+};
+
+const unSpanned = (t) => {
+  const spanRegex = new RegExp(
+    `<span style="color: blue;"[^>]*>(.*?)</span>`,
+    "g"
+  );
+  return t.replace(spanRegex, "$1");
+};
